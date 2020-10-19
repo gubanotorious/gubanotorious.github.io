b5c52346b771dc527a812624baad5762e5ab507a
Alex Guba <gubanotorious@gmail.com>
2020-10-19 14:45:11 -0400

    Publish test

---
+++ scripts/news.js
@@ -0,0 +1,27 @@
+$(async function(){
+    let articles = await getNewsArticles();
+    let $container = $("#news-container");
+    for(const article of articles){
+        let html = `
+            <div class="news-article" onclick="window.location.href='${article.FeedUrl}';">
+                <div class="news-image"><img src="${article.ImageUrl}"></img></div>
+                <div class="news-info">
+                    <div>${article.PublishDate}</div>
+                    <div>${article.Title}</div>
+                </div>
+            </div>
+        `;
+        $container.append(html);
+    }
+});
+
+async function getNewsArticles(){
+    return new Promise((resolve, reject) => {
+        $.get( "https://bridge-protocol.azurewebsites.net/api/news", function( data ) {
+            resolve(data.items);
+        })
+        .then(() => {
+            throw new Error('Could not retrieeve articles');
+        })
+    })
+}
